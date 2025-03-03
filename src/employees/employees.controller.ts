83f97f6d14a87f1f8047d08062ebf9f33f7f3edd
Walter Sorto <wsorto89@gmail.com>
2025-03-03 15:27:26 -0800

    added logging, cos, api prefix

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -7,13 +7,16 @@ import {
   Param,
   Delete,
   Query,
+  Ip,
 } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { Prisma } from '@prisma/client';
+import { MyLoggerService } from 'src/my-logger/my-logger.service';
 
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
+  private readonly logger = new MyLoggerService(EmployeesController.name)
 
   @Post()
   create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
@@ -21,7 +24,8 @@ export class EmployeesController {
   }
 
   @Get()
-  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+  findAll(@Ip() ip: string,@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    this.logger.log(`Request for ALL Employees\t${ip}`, EmployeesController.name)
     return this.employeesService.findAll(role);
   }
 
